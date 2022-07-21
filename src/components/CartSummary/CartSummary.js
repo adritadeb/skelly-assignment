f7031d4141b8b4f5c09d9fdf3dea57309fb4e10f
adritadeb <adritad2019@gmail.com>
2022-07-21 13:19:46 +0600

    Cart section done

--- src/components/CartSummary/CartSummary.js
+++ src/components/CartSummary/CartSummary.js
@@ -21,27 +21,34 @@ const CartSummary = () => {
                         <div>
                             <p className='font-medium text-gray-600'>T-Shirt</p>
                             <p className='font-medium text-gray-600'>Summer Vibes</p>
-                            <p className='font-medium text-gray-200 pt-2'>#261311</p>
+                            <p className='font-medium text-gray-300 pt-2 text-sm'>#261311</p>
                         </div>
                     </div>
                     <p className='font-medium text-gray-600 ml-12'>$89.99</p>
                 </div>
             </section>
-            <section className='mt-6'>
+            <section className='mt-5'>
                 <div className='flex items-center'>
                     <div className='flex items-center'>
                         <div className='border-2 rounded-full p-2 border-emerald-400 mr-4'><ShoppingCartIcon className='h-6'></ShoppingCartIcon></div>
                         <div>
                             <p className='font-medium text-gray-600'>Basic Slim</p>
                             <p className='font-medium text-gray-600'>Fit T-Shirt</p>
-                            <p className='font-medium text-gray-200 pt-2'>#212315</p>
+                            <p className='font-medium text-gray-300 pt-2 text-sm'>#212315</p>
                         </div>
                     </div>
-                    <p className='font-medium text-gray-600 ml-12'>$69.99</p>
+                    <p className='font-medium text-gray-600 ml-20'>$69.99</p>
                 </div>
             </section>
             <div>
-                <button className='uppercase rounded-full border-2 px-8 py-3 ml-3'>Sign Up</button>
+                <button className='rounded-full border-0 px-8 py-3 ml-3 mt-8 flex bg-gray-200'>
+                    <p className='mr-10 text-gray-500'>Total cost</p>
+                    <p>$159.98</p>
+                </button>
+            </div>
+            <div className='flex items-center text-gray-600 mt-10 ml-7 text-sm'>
+                <TruckIcon className='h-5 mr-6'></TruckIcon>
+                <p><span className='block'>You are <span className='text-black'>$30.02</span> away</span>from free shipping!</p>
             </div>
         </div>
     );

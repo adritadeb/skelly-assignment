ca67b6504445bd39f2ac40cb25f08423eb8023d4
adritadeb <adritad2019@gmail.com>
2022-07-21 10:35:32 +0600

    Delivery method added

--- src/components/PaymentMethod/PaymentMethod.js
+++ src/components/PaymentMethod/PaymentMethod.js
@@ -5,19 +5,19 @@ const PaymentMethod = () => {
         <div>
             <p className='font-medium text-gray-600 pt-20 pb-5'>Payment method</p>
             <div className='grid grid-cols-3 gap-2'>
-                <button className='rounded-full border-2 px-6 py-3'>PayPal</button>
-                <button className='rounded-full border-2 px-6 py-3'>Visa</button>
-                <button className='rounded-full border-2 px-6 py-3'>Most</button>
-                <button className='rounded-full border-2 px-6 py-3'>payment</button>
-                <button className='rounded-full border-2 px-6 py-3'>discover</button>
-                <button className='rounded-full border-2 px-6 py-3'>deal</button>
+                <button className='rounded-3xl border-2 px-6 py-3'>PayPal</button>
+                <button className='rounded-3xl border-2 px-6 py-3'>Visa</button>
+                <button className='rounded-3xl border-2 px-6 py-3'>Most</button>
+                <button className='rounded-3xl border-2 px-6 py-3'>payment</button>
+                <button className='rounded-3xl border-2 px-6 py-3'>discover</button>
+                <button className='rounded-3xl border-2 px-6 py-3'>deal</button>
             </div>
             <p className='font-medium text-gray-600 pt-12 pb-5'>Delivery method</p>
             <div className='grid grid-cols-2 gap-2'>
-                <button className='rounded-full border-2 px-6 py-3'>InPost</button>
-                <button className='rounded-full border-2 px-6 py-3'>dpd</button>
-                <button className='rounded-full border-2 px-6 py-3'>dhl</button>
-                <button className='rounded-full border-2 px-6 py-3'>Fedex</button>
+            <button className='rounded-full border-2 px-6 py-3'>InPost</button>
+                <button className='rounded-3xl border-2 px-6 py-3'>dpd</button>
+                <button className='rounded-3xl border-2 px-6 py-3'>dhl</button>
+                <button className='rounded-3xl border-2 px-6 py-3'>Fedex</button>
             </div>
         </div>
     );
